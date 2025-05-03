5ed981fe66a3f7f3639725e05264bf7e5f84a9b9
Arabella Valerio <arabellavalerio24@gmail.com>
2025-05-03 19:26:57 +0800

    update 2

--- Hospital-PHP/backend/admin/assets/inc/footer1.php
+++ Hospital-PHP/backend/admin/assets/inc/footer1.php
@@ -1,3 +1,3 @@
 <footer class="footer footer-alt">
-    2020 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By Martin Mbithi Nzilani</a>
+    2025 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By Group 5 CHART</a>
 </footer>
